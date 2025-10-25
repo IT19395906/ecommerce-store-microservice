70b5be8b3e2524c5bbf0cf3c66128c42f190ef6e
IT19395906 <rwijayabandu@gmail.com>
2025-10-25 20:14:28 +0530

    getItems(), getItemById() complete, use modelMapper for dto to entity conversion

--- microservices/inventory-service/src/main/java/com/example/inventoryservice/Impl/InventoryServiceImpl.java
+++ microservices/inventory-service/src/main/java/com/example/inventoryservice/Impl/InventoryServiceImpl.java
@@ -1,11 +1,15 @@
 package com.example.inventoryservice.Impl;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.inventoryservice.dto.InventoryDto;
+import com.example.inventoryservice.entity.Inventory;
 import com.example.inventoryservice.repo.InventoryRepository;
 import com.example.inventoryservice.service.InventoryService;
 
@@ -15,16 +19,19 @@ public class InventoryServiceImpl implements InventoryService{
     @Autowired
     private InventoryRepository inventoryRepository;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Override
     public List<InventoryDto> getItems() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getItems'");
+        List<Inventory> itemList = inventoryRepository.findAll();
+        return itemList.stream().map(item-> modelMapper.map(item, InventoryDto.class)).collect(Collectors.toList());
     }
 
     @Override
     public InventoryDto getItemById(Integer itemId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getItemById'");
+        Optional<Inventory> item = inventoryRepository.findById(itemId);
+        return modelMapper.map(item,InventoryDto.class);
     }
 
     @Override
