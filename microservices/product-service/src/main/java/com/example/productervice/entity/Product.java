3a393e0289b86b9a4b4e6c3a3897e6595e7e957a
IT19395906 <rwijayabandu@gmail.com>
2025-10-27 00:03:15 +0530

    product pom xml updated with mysql jdbc dependency

--- microservices/product-service/src/main/java/com/example/productervice/entity/Product.java
+++ microservices/product-service/src/main/java/com/example/productervice/entity/Product.java
@@ -1,7 +1,13 @@
 package com.example.productervice.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@NoArgsConstructor
+@Data
 public class Product {
 
     @Id
