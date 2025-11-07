1a8eb14c89c9429eaa5a07164c72a2887d39ef72
IT19395906 <rwijayabandu@gmail.com>
2025-11-07 23:52:37 +0530

    table name order change orders cos its reserved by sql @Table(name=orders)

--- microservices/order-service/src/main/java/com/example/orderservice/entity/Order.java
+++ microservices/order-service/src/main/java/com/example/orderservice/entity/Order.java
@@ -2,12 +2,14 @@ package com.example.orderservice.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 @Data
+@Table(name = "orders")
 public class Order {
 
     @Id
