744923afc03d91e65446a49f87c8f7e0246939e9
IT19395906 <rwijayabandu@gmail.com>
2025-11-17 13:32:18 +0530

    kafka issue fixed

--- microservices/inventory-service/src/main/java/com/example/inventoryservice/Impl/Consumer.java
+++ microservices/inventory-service/src/main/java/com/example/inventoryservice/Impl/Consumer.java
@@ -11,7 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class Consumer {
     
-    @KafkaListener(topics = "${spring.kafka.template.default-topic}", groupId = "${spring.kafka.consumer.groupId}")
+    @KafkaListener(topics = "${spring.kafka.template.default-topic}", groupId = "${spring.kafka.consumer.group-id}")
     
     public void receive(CommonResponseDto commonResponseDto){
         log.info("Receiving order from topic {}", commonResponseDto);
